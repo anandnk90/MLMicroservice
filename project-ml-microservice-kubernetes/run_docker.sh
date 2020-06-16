5108f3d56352025d2ebe49a0295713ff20e0c837
Anand Nandakumar <anandnk90@gmail.com>
2020-06-16 13:38:18 -0400

    Updated app.py to use correct version of sklearn; updated docker run script

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,11 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+app="udacityml/app"
+docker build -t ${app} .
 
 # Step 2: 
-# List docker images
+docker image ls
 
 # Step 3: 
-# Run flask app
+docker run -d -p 8100:80 ${app}
