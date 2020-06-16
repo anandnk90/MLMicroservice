c820e966769f2afc3d44eb64637701c9d7c0f42d
Anand Nandakumar <anandnk90@gmail.com>
2020-06-16 17:27:05 -0400

    upload docker updated

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,19 @@
-#!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-# Assumes that an image is built via `run_docker.sh`
+#to run: ./upload_docker.sh <<dockerpath>> <<docker image>>
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+#dockerpath=anandnk90/udacitymlapp
+dockerpath=${1}
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+#docker login --username=anandnk90 --email=anandnk90@gmail.com
+echo -n Password:
+read -s passphrase
+docker login --username anandnk90 --password $passphrase
+docker tag ${2} $dockerpath:udacitymlapp
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
