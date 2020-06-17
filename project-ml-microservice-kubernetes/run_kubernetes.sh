64fa51a64660289211d1cb212d3062b4ae84e4b6
Anand Nandakumar <anandnk90@gmail.com>
2020-06-16 21:52:26 -0400

    kubernetes wip

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,14 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=${1}
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp --image=$dockerpath:udacitymlapp --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
