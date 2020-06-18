d33cc8a74b58c7bf27071ab55205c27fdbf2a9a9
Anand Nandakumar <anandnk90@gmail.com>
2020-06-18 18:02:56 -0400

    Updated kubernetes run and dependencies

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-#dockerpath=anandnk90/udacitymlapp
-dockerpath=${1}
+dockerpath=anandnk90/udacitymlapp
+#dockerpath=${1}
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-#Creates a pod with the container
-kubectl create deployment mlapp --image=$dockerpath:udacitymlapp
-kubectl run mlapp --image=$dockerpath:udacitymlapp --port=8000
+#Create deployment
+#kubectl create deployment mlapp --image=$dockerpath:udacitymlapp
+kubectl apply -f kubernetesfiles/kubernetesdeployment.yml
 
 # Step 3:
 # List kubernetes pods
@@ -20,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl apply -f kubernetesfiles/kubernetesservice.yml
